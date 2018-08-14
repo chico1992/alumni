4e0cd7b60a3075bbfe67a302e9a7c089300f6c90
chico1992 <cedric.correia.alves@gmail.com>
2018-08-14 11:09:30 +0200

    created the form for the invitation

---
+++ alumni/src/Controller/AdminController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use App\Service\MailSender;
+
+
+class AdminController extends Controller
+{
+    public function invitation(MailSender $mailSender)
+    {
+        
+    }
+}
