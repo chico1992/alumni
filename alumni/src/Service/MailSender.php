06e458c6b39537df8a4821e79a134fd55fea9522
chico1992 <cedric.correia.alves@gmail.com>
2018-08-14 10:41:42 +0200

    created the mailsender Service and created a templete for the registration mail

---
+++ alumni/src/Service/MailSender.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Invitation;
+
+
+class MailSender
+{
+    private $twig;
+    private $mailer;
+
+    public function __construct(
+        \Twig_Environment $twig,
+        \Swift_Mailer $mailer
+    )
+    {
+        $this->twig = $twig;
+        $this->mailer = $mailer;
+    }
+
+    public function sendRegistration(Invitation $invitation)
+    {
+        $message = (new \Swift_Message('Registration Mail'))
+            ->setFrom('cedric.correia.alves@gmail.com')
+            ->setTo($invitation->getEmail())
+            ->setBody(
+                $this->twig->render(
+                    'email/invitation.html.twig',
+                    array('invitation' => $invitation)
+                ),
+                'text/html'
+            )
+            ->addPart(
+                $this->twig->render(
+                    'email/invitation.txt.twig',
+                    array('invitation' => $invitation)
+                ),
+                'text/plain'
+            )
+        ;
+
+        $this->mailer->send($message);
+    }
+}
