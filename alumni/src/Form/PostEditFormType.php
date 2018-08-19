fbc64e1a2dbcf5c4594f872df1117240cfdf56e5
mklepper <klepper.max@gmail.com>
2018-08-19 22:47:39 +0200

    user edit for posts and comments

---
+++ alumni/src/Form/PostEditFormType.php
@@ -0,0 +1,41 @@
+<?php
+namespace App\Form;
+use App\Entity\Post;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\VisibilityGroup;
+class PostEditFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title',
+                TextType::class,
+                ['label' => 'Title for your post']
+            )   
+            ->add('content',
+                TextareaType::class,
+                ['label' => 'Enter the content of your post']
+            )
+        ;
+        if ($options['standalone']) {
+            $builder->add(
+                'submit', 
+                SubmitType::class,
+                ['attr'=>['class'=> 'btn btn-lg btn-dark btn-block']]
+            );
+        }
+    }
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Post::class,
+            'standalone' => false,
+        ]);
+    }
+}
