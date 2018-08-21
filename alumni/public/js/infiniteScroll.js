afc11e327f426d598ddf0466afdd88bfaec52db1
mklepper <klepper.max@gmail.com>
2018-08-21 22:57:52 +0200

    infinite scroll loading posts: done

--- alumni/public/js/infiniteScroll.js
+++ alumni/public/js/infiniteScroll.js
@@ -9,6 +9,38 @@ $(document).ready(function() {
 
 	var win = $(window);
 
+	function postCreator(post)
+	{
+		let mainDiv = $('<div class="col-md-12"></div>');
+		let postDiv = $('<div class="card mb-3"></div>');
+		let postHeader = $('<div class="card-footer text-muted"></div>');
+		let postHeaderIcons = $('<div class="icons"></div>');
+
+		let postBody = $('<div class="card-body"></div>');
+		let postBodyContent = $('<div class="media-body"></div>');
+		let postBodyContentHeader = $('<div class="media-body"></div>');
+
+		postHeader.append($('<p class="group-name font-weight-light font-italic d-inline">'+post.visibility.label+'</p>'+post.creationDate+'<a href="#" class="deco-none">'+post.author.firstname+'</a>'));
+		postHeaderIcons.append($('<a href="#" class="deco-none"><i class="fas fa-pencil-alt mr-2 d-inline"></i></a>'));
+		postHeaderIcons.append($('<a href="#" class="deco-none"><i class="fas fa-trash-alt mr-2 d-inline"></i></a>'));
+		postHeaderIcons.append($('<a href="#" class="deco-none"><i class="fas fa-flag mr-2 d-inline"></i></a>'));
+		postHeader.append(postHeaderIcons);
+
+		postBodyContentHeader.append($('<img class="mr-3 mb-3 rounded-circle" src="http://placehold.it/40x40" alt=""></img>'));
+		postBodyContentHeader.append($('<h2 class="card-title">'+post.title+'</h2>'));
+		postBody.append(postBodyContentHeader);
+		postBodyContent.append($(' <p class="card-text">'+post.content+'</p>'));
+		postBody.append(postBodyContent);
+
+		postDiv.append(postHeader);
+		postDiv.append(postBody);
+
+		mainDiv.append(postDiv);
+
+		return mainDiv;
+		
+	}
+
 	$.get("http://localhost/posts/"+timestamp).done(function(result){
 		let X = result.length;
 		
@@ -16,7 +48,9 @@ $(document).ready(function() {
 		result.forEach(post => {
 			
 				posts.push(post)
-				ul.append($('<li>').text(post.creationDate));
+				//ul.append($('<li>').text(post.creationDate));
+				$(".row").append(postCreator(post))
+				console.log(post);
 
 		});
 		
@@ -39,10 +73,9 @@ $(document).ready(function() {
 					res.forEach(post => {
 						
 						posts.push(post)
-						ul.append($('<li>').text(post.creationDate));
+						//ul.append($('<li>').text(post.creationDate));
+						$(".row").append(postCreator(post))
 						
-					
-				
 					});
 				} 
 				else 
